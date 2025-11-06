a12032126cb4aaaf2fac928c2378f8acf0dd7b94
Hernan Arregoces <harregoces@gmail.com>
2025-11-06 11:49:38 -0500

    Add Indonesian and Russian language strings for Tutor-IA plugin

---
+++ lang/id/local_dttutor.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Indonesian language strings for Tutor-IA plugin.
+ *
+ * @package    local_dttutor
+ * @copyright  2025 Datacurso
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['apitoken'] = 'Token autentikasi';
+$string['apitoken_desc'] = 'Token autentikasi untuk API Tutor-IA';
+$string['apiurl'] = 'URL API Tutor-IA';
+$string['apiurl_desc'] = 'URL dasar API Tutor-IA (contoh: https://plugins-ai-dev.datacurso.com)';
+$string['avatar'] = 'Avatar Tutor-IA';
+$string['avatar_desc'] = 'Pilih avatar yang akan ditampilkan pada tombol chat mengambang Tutor-IA. Jika tidak ada yang dipilih atau file tidak ada, Avatar 1 akan digunakan secara default.';
+$string['avatar_position'] = 'Posisi avatar';
+$string['avatar_position_desc'] = 'Pilih sudut tempat tombol chat mengambang Tutor-IA akan ditampilkan. Secara default muncul di sudut kanan bawah.';
+$string['cachedef_sessions'] = 'Cache untuk sesi chat Tutor-IA';
+$string['close'] = 'Tutup Tutor IA';
+$string['dttutor:use'] = 'Gunakan Tutor-IA';
+$string['enabled'] = 'Aktifkan Chat';
+$string['enabled_desc'] = 'Aktifkan atau nonaktifkan chat Tutor-IA secara global';
+$string['error_api_not_configured'] = 'Konfigurasi API tidak ada. Silakan periksa pengaturan Anda.';
+$string['error_api_request_failed'] = 'Kesalahan permintaan API: {$a}';
+$string['error_http_code'] = 'Kesalahan HTTP {$a}';
+$string['error_invalid_api_response'] = 'Respons API tidak valid';
+$string['open'] = 'Buka Tutor IA';
+$string['pluginname'] = 'Tutor IA';
+$string['position_left'] = 'Sudut kiri bawah';
+$string['position_right'] = 'Sudut kanan bawah';
+$string['sendmessage'] = 'Kirim pesan';
+$string['sessionnotready'] = 'Sesi Tutor-IA belum siap. Silakan coba lagi.';
+$string['student'] = 'Siswa';
+$string['teacher'] = 'Guru';
+$string['typemessage'] = 'Ketik pesan Anda...';
+$string['unauthorized'] = 'Akses tidak sah';
+$string['welcomemessage'] = 'Halo! Saya adalah asisten AI Anda. Bagaimana saya dapat membantu Anda hari ini?';
---
+++ lang/ru/local_dttutor.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Russian language strings for Tutor-IA plugin.
+ *
+ * @package    local_dttutor
+ * @copyright  2025 Datacurso
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['apitoken'] = 'Токен аутентификации';
+$string['apitoken_desc'] = 'Токен аутентификации для API Tutor-IA';
+$string['apiurl'] = 'URL API Tutor-IA';
+$string['apiurl_desc'] = 'Базовый URL API Tutor-IA (например: https://plugins-ai-dev.datacurso.com)';
+$string['avatar'] = 'Аватар Tutor-IA';
+$string['avatar_desc'] = 'Выберите аватар для отображения на плавающей кнопке чата Tutor-IA. Если ничего не выбрано или файл не существует, по умолчанию будет использоваться Аватар 1.';
+$string['avatar_position'] = 'Положение аватара';
+$string['avatar_position_desc'] = 'Выберите угол, где будет отображаться плавающая кнопка чата Tutor-IA. По умолчанию она появляется в правом нижнем углу.';
+$string['cachedef_sessions'] = 'Кэш для сеансов чата Tutor-IA';
+$string['close'] = 'Закрыть Tutor IA';
+$string['dttutor:use'] = 'Использовать Tutor-IA';
+$string['enabled'] = 'Включить чат';
+$string['enabled_desc'] = 'Включить или отключить чат Tutor-IA глобально';
+$string['error_api_not_configured'] = 'Конфигурация API отсутствует. Пожалуйста, проверьте настройки.';
+$string['error_api_request_failed'] = 'Ошибка запроса API: {$a}';
+$string['error_http_code'] = 'Ошибка HTTP {$a}';
+$string['error_invalid_api_response'] = 'Недействительный ответ API';
+$string['open'] = 'Открыть Tutor IA';
+$string['pluginname'] = 'Tutor IA';
+$string['position_left'] = 'Левый нижний угол';
+$string['position_right'] = 'Правый нижний угол';
+$string['sendmessage'] = 'Отправить сообщение';
+$string['sessionnotready'] = 'Сеанс Tutor-IA не готов. Пожалуйста, попробуйте снова.';
+$string['student'] = 'Студент';
+$string['teacher'] = 'Преподаватель';
+$string['typemessage'] = 'Введите ваше сообщение...';
+$string['unauthorized'] = 'Несанкционированный доступ';
+$string['welcomemessage'] = 'Здравствуйте! Я ваш AI-помощник. Как я могу помочь вам сегодня?';
