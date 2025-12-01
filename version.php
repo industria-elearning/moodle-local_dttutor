236a802678e28cd853a5c68d0256ed95c83a09e0
wilber-buendata <wilber@buendata.com>
2025-12-01 17:05:41 -0500

    Bump plugin version to 2025120103

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_dttutor';
-$plugin->version = 2025120102;
+$plugin->version = 2025120103;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.9.0';
